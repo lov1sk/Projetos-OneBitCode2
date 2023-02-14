d37434858225e84e649e9144280f14b03389d597
Lucas Ribeiro Santana <lucassantana00@gmail.com>
2023-02-13 22:13:56 -0300

    Criando um script para manipular arquivo

--- 9 - NodeJS e mongoDB/io.js
+++ 9 - NodeJS e mongoDB/io.js
@@ -1,7 +1,13 @@
 const process = require("process");
 
+//Comando para escrever no console a msg, parecido com o console.log
 process.stdout.write("Qual o seu nome?\n");
 
+/**
+ * Metodo para entrar com dados no node, primeiro é especificado o tipo de entrada
+ * (no caso dados) depois a variavel que sera armazenado tudo o que for escrito no
+ * console (no caso armazena na variavel teclado)
+ */
 process.stdin.on("data", (teclado) => {
   process.stdout.write("\n==> Texto inserido: " + teclado);
   process.exit();
