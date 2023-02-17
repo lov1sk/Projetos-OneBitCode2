a7344ec650ed36a164d99a8641485b52a2266e8a
Lucas Ribeiro Santana <lucassantana00@gmail.com>
2023-02-16 23:45:31 -0300

    Finalização dos arquivos app e checklist.js 16/02/23

--- 9 - NodeJS e mongoDB/toDoList/app.js
+++ 9 - NodeJS e mongoDB/toDoList/app.js
@@ -1,12 +1,28 @@
 //Importando o express do node_modules
 const express = require("express");
 
+const checklists = require("./src/routes/checklist.js");
+
 /* Salvando na constante app todos os
  * metodos do express, no caso para ser
  * chamados na aplicação */
 const app = express();
 
+const log = (req, res, next) => {
+  console.log(`Data agora : ${Date.now()}`);
+  next();
+};
+
+/** Uso de um middleware que processa se possui algum json no body da requisição e salva em req.body
+ * Neste caso de midleware, ele deixa sempre em maos a informação do json no body da requisição para ser usada */
 app.use(express.json());
+
+//app.use(log);
+
+/**
+ * Utilizando o middleware router feito para lidar com a rota de /checklists, definido no app.user
+ */
+app.use("/checklists", checklists);
 /**
  * Criando um novo endpoint no root da aplicaçõa, e caso caia nele
  * o mesmo devolvera para o brownser o "MyList"
@@ -20,7 +36,9 @@ app.get("/", (req, res) => {
  * Criação de endpoint /json que retorna um objeto json para
  * quem fizer essa requisição
  */
+
 app.get("/json", (req, res) => {
+  console.log(req.body);
   res.json({ title: "Tarefa 1", done: true });
 });
 
@@ -31,6 +49,7 @@ app.get("/teste", (req, res) => {
  * Definindo que o servidor irá ouvir todas as requisições
  * localhost que cair na porta 3000
  */
+
 app.listen(3000, () => {
   console.log("Servidor Ativo!");
 });
---
+++ 9 - NodeJS e mongoDB/toDoList/src/routes/checklist.js
@@ -0,0 +1,28 @@
+//Importando o express do node_modules
+const express = require("express");
+
+//Criando uma constante router para a rota /checklists
+const router = express.Router();
+
+/**
+ * Metodo para quando a rota /checklists for chamada no postman ou browser
+ * ele retornar um console log
+ */
+router.get("/", (req, res) => {
+  console.log("teste");
+  res.send();
+});
+/**
+ * Metodo para retornar no response o id passado em /checklists/id
+ * o ":" significa que a rota em questão recebera um parametro
+ */
+router.get("/:id", (req, res) => {
+  res.send(`ID: ${req.params.id}`);
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.status(200).send(req.body);
+});
+//Exportando a rota /checklists
+module.exports = router;
