205df5750b5d3a54dd905390c33c017f88de6ee2
Lucas Ribeiro Santana <lucassantana00@gmail.com>
2023-02-20 09:18:17 -0300

    Mudanças no arquivo app.js para deixa-lo mais otimizado

--- 9 - NodeJS e mongoDB/toDoList/app.js
+++ 9 - NodeJS e mongoDB/toDoList/app.js
@@ -1,7 +1,7 @@
 //Importando o express do node_modules
 const express = require("express");
-
 const checklists = require("./src/routes/checklist.js");
+const test = require("./src/routes/test.js");
 
 /* Salvando na constante app todos os
  * metodos do express, no caso para ser
@@ -23,28 +23,8 @@ app.use(express.json());
  * Utilizando o middleware router feito para lidar com a rota de /checklists, definido no app.user
  */
 app.use("/checklists", checklists);
-/**
- * Criando um novo endpoint no root da aplicaçõa, e caso caia nele
- * o mesmo devolvera para o brownser o "MyList"
- */
-app.get("/", (req, res) => {
-  console.log("Bem Vindo!");
-  res.send("<h1> My List </h1>");
-});
+app.use("/test", test);
 
-/**
- * Criação de endpoint /json que retorna um objeto json para
- * quem fizer essa requisição
- */
-
-app.get("/json", (req, res) => {
-  console.log(req.body);
-  res.json({ title: "Tarefa 1", done: true });
-});
-
-app.get("/teste", (req, res) => {
-  res.send("<h1> Parabens, voce fez uma nova requisição ao /teste </h1>");
-});
 /**
  * Definindo que o servidor irá ouvir todas as requisições
  * localhost que cair na porta 3000
